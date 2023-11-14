bf6991b35175bac3a7092346cf8104c7b85e34f4
Oliver Skivington <Oliver.Skivington@criticalsoftware.com>
2023-11-14 17:14:23 +0000

    WOM-152 Add method to get types and enum strings
    Adds two methods to the sculib, get_attribute_data_type() and
    get_enum_strings() that will return the data type or a list of possible
    enum strings respectively.

--- src/disq/sculib.py
+++ src/disq/sculib.py
@@ -498,6 +498,41 @@ class scu:
         logger.info(info)
         return list(self.attributes.keys())
 
+    def get_attribute_data_type(self, attribute: str) -> str:
+        """
+        Get the data type for the given node. Returns "Boolean"/"Double"/"Enumeration",
+        for the respective types or "Unknown" for a not yet known type.
+        """
+        node = self.nodes[attribute]
+        dt_id = asyncio.run_coroutine_threadsafe(node.read_data_type(), self.event_loop).result()
+        dt_node = self.connection.get_node(dt_id)
+        dt_node_info = asyncio.run_coroutine_threadsafe(dt_node.read_browse_name(), self.event_loop).result()
+        dt_name = dt_node_info.Name
+        if dt_name == "Boolean" or dt_name == "Double":
+            return dt_name
+        
+        supertypes = asyncio.run_coroutine_threadsafe(asyncua.common.ua_utils.get_node_supertypes(dt_node), self.event_loop).result()
+        for super in supertypes:
+            super_node = self.connection.get_node(super)
+            super_node_info = asyncio.run_coroutine_threadsafe(super_node.read_browse_name(), self.event_loop).result()
+            super_node_name = super_node_info.Name
+            if super_node_name == "Enumeration":
+                return super_node_name
+
+        return "Unknown"
+    
+    def get_enum_strings(self, enum_node: str) -> list[str]:
+        """
+        Get a list of enumeration strings where the index of the list matches the enum value.
+        enum_node MUST be the name of an Enumeration type node (see get_attribute_data_type()).
+        """
+        node = self.nodes[enum_node]
+        dt_id = asyncio.run_coroutine_threadsafe(node.read_data_type(), self.event_loop).result()
+        dt_node = self.connection.get_node(dt_id)
+        dt_node_def = asyncio.run_coroutine_threadsafe(dt_node.read_data_type_definition(), self.event_loop).result()
+        
+        return [Field.Name if Field.Value == index else logger.error("Enum fields out of order") for index, Field in enumerate(dt_node_def.Fields)]
+        
     def subscribe(self, attributes: Union[str, list[str]] = hn_opcua_tilt_sensors, period: int = 100, data_queue: queue.Queue = None) -> int:
         if data_queue is None:
             data_queue = self.subscription_queue
