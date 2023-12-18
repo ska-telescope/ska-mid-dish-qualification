2cfee67020ec1cdeb7cbb885f3ce9c1b7ce9017a
Oliver Skivington <Oliver.Skivington@criticalsoftware.com>
2023-12-18 15:49:17 +0000

    WOM-139: Node tree from input XML
    Serval class can create a node tree from an input xml.

---
+++ tools/server_validator/server_validator.py
@@ -0,0 +1,126 @@
+import argparse
+import os
+import sys
+import yaml
+import xml.etree.ElementTree as ET
+import xml.etree
+import asyncua
+import re
+
+
+class Serval:
+    def __init__(self):
+        self.config = None
+        self.xml = None
+        self.missing = {}
+        self.extra = {}
+        self.plc_prg_string = "1:PLC_PRG"
+
+    def _get_browse_name_from_xml_element(self, element: ET.Element):
+        browse_name = None
+        if "BrowseName" in element.attrib:
+            browse_name = element.attrib["BrowseName"]
+
+        return browse_name
+
+    def _get_element_from_node_string(self, node_string: str):
+        for ele in self.xml.iter():
+            if "NodeId" in ele.attrib:
+                if ele.attrib["NodeId"] == node_string:
+                    return ele
+
+        return None
+
+    def _get_children_from_xml_element_recursive(self, element: ET.Element):
+        children_node_strings = []
+
+        for ele in element.iter():
+            if ele.tag == self.xmlns + "Reference":
+                if "ReferenceType" in ele.attrib:
+                    if ele.attrib["ReferenceType"] == "HasComponent":
+                        if (
+                            "IsForward" not in ele.attrib
+                            or ele.attrib["IsForward"] == "true"
+                        ):
+                            children_node_strings.append(ele.text)
+
+        children = {}
+        for child_string in children_node_strings:
+            child_ele = self._get_element_from_node_string(child_string)
+            if child_ele is not None:
+                children[child_ele] = {
+                    "browse_name": self._get_browse_name_from_xml_element(child_ele),
+                    "children": self._get_children_from_xml_element_recursive(
+                        child_ele
+                    ),
+                }
+
+        return children
+
+    def validate(self, xml_file: str, server_config: str):
+        print(f"Using xml file: {xml_file}, and config file: {server_config}")
+        with open(server_config, "r") as f:
+            try:
+                self.config = yaml.safe_load(f.read())
+            except Exception as e:
+                print(e)
+                sys.exit(
+                    f"ERROR: Unable to parse server configuration file {server_config}."
+                )
+
+        print(self.config)
+        self.xml = ET.parse(xml_file)
+        root = self.xml.getroot()
+        self.xmlns = re.split("(^.*})", root.tag)[1]
+        plc_prg = None
+        # Find the PLC_PRG node in the xml
+        for element in root.iter():
+            if "BrowseName" in element.attrib:
+                if element.attrib["BrowseName"] == self.plc_prg_string:
+                    plc_prg = element
+
+        if plc_prg is None:
+            sys.exit(f"ERROR: Could not find PLC_PRG node in input XML")
+
+        # Recursively build input tree from xml
+        self.xml_tree = {
+            plc_prg: {
+                "browse_name": self._get_browse_name_from_xml_element(plc_prg),
+                "children": self._get_children_from_xml_element_recursive(plc_prg),
+            }
+        }
+        print(self.xml_tree)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Dish Structure Server Validator",
+        description="Validates an OPCUA server against an XML.",
+    )
+    parser.add_argument(
+        "-x",
+        "--xml",
+        help="XML file to validate against.",
+        required=True,
+        nargs=1,
+        dest="xml",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="YAML file specifying OPCUA server configuration.",
+        required=True,
+        nargs=1,
+        dest="config",
+    )
+    args = parser.parse_args()
+    xml = args.xml[0]
+    config = args.config[0]
+    if not os.path.isfile(xml):
+        sys.exit(f"ERROR: Could not find file {xml}")
+
+    if not os.path.isfile(config):
+        sys.exit(f"ERROR: Could not find file {config}")
+
+    validator = Serval()
+    validator.validate(xml, config)
