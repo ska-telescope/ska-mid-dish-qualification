c2dbdc49f683959f787a0a0556bc9dfa1164480e
Oliver Skivington <Oliver.Skivington@criticalsoftware.com>
2023-12-18 17:34:37 +0000

    WOM-193: report missing nodes

--- tools/server_validator/server_validator.py
+++ tools/server_validator/server_validator.py
@@ -6,14 +6,15 @@ import xml.etree.ElementTree as ET
 import xml.etree
 import asyncua
 import re
+from disq import sculib
 
 
 class Serval:
     def __init__(self):
         self.config = None
         self.xml = None
-        self.missing = {}
-        self.extra = {}
+        self.missing = []
+        self.extra = []
         self.plc_prg_string = "1:PLC_PRG"
 
     def _get_browse_name_from_xml_element(self, element: ET.Element):
@@ -83,13 +84,80 @@ class Serval:
             sys.exit(f"ERROR: Could not find PLC_PRG node in input XML")
 
         # Recursively build input tree from xml
-        self.xml_tree = {
-            plc_prg: {
-                "browse_name": self._get_browse_name_from_xml_element(plc_prg),
-                "children": self._get_children_from_xml_element_recursive(plc_prg),
+        # self.xml_tree = {
+        #    plc_prg: {
+        #        "browse_name": self._get_browse_name_from_xml_element(plc_prg),
+        #        "children": self._get_children_from_xml_element_recursive(plc_prg),
+        #    }
+        # }
+        # sculib does not include plc_prg in nodes
+        self.xml_tree = self._get_children_from_xml_element_recursive(plc_prg)
+        # extras for development
+        axis_select_type = None
+        for element in root.iter():
+            if "BrowseName" in element.attrib:
+                if element.attrib["BrowseName"] == "1:AxisSelectType":
+                    axis_select_type = element
+        self.xml_tree.update(
+            {
+                root: {
+                    "browse_name": "1:test1",
+                    "children": {
+                        root: {
+                            "browse_name": "1:test2",
+                            "children": {
+                                axis_select_type: {
+                                    "browse_name": "1:test3",
+                                    "children": {},
+                                }
+                            },
+                        }
+                    },
+                },
+                axis_select_type: {
+                    "browse_name": "1:axis1",
+                    "children": {
+                        root: {"browse_name": "1:axis1.1", "children": {}},
+                        axis_select_type: {"browse_name": "1:axis1.2", "children": {}},
+                    },
+                },
             }
-        }
-        print(self.xml_tree)
+        )
+        # print(self.xml_tree)
+
+        self.hll = sculib.scu(
+            host=self.config["connection"]["address"],
+            port=self.config["connection"]["port"],
+            endpoint=self.config["connection"]["endpoint"],
+            namespace=self.config["connection"]["namespace"],
+        )
+        # print(self.hll.nodes)
+        for input_node in self.xml_tree:
+            self._check_sculib_against_xml_recursive(self.xml_tree[input_node], "")
+
+        print("OPCUA Server is missing the following nodes:")
+        for node in self.missing:
+            print(node)
+
+    def _check_sculib_against_xml_recursive(self, entry, path):
+        name = path + entry["browse_name"].split(":")[1]
+        if name in self.hll.nodes:
+            print(f"found node for {name}")
+            for child in entry["children"]:
+                self._check_sculib_against_xml_recursive(
+                    entry["children"][child], name + "."
+                )
+        else:
+            self.missing.append(name)
+            self._walk_xml_tree_missing_recursive(entry["children"], name + ".")
+
+    def _walk_xml_tree_missing_recursive(self, children, path):
+        for child in children:
+            name = path + children[child]["browse_name"].split(":")[1]
+            self.missing.append(name)
+            self._walk_xml_tree_missing_recursive(
+                children[child]["children"], name + "."
+            )
 
 
 if __name__ == "__main__":
