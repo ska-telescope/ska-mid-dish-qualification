e70af3950f37b52fbc52cbd0ea413a780f13c133
Ulrik Pedersen <ukp@observatorysciences.co.uk>
2024-08-01 20:41:11 +0100

    WOM-409: rename sculib.SCU class to _SCU
    In preparation for creating the SCU() generator method

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -19,7 +19,7 @@ import h5py
 import pytest
 
 from disq.logger import Logger
-from disq.sculib import SCU
+from disq.sculib import _SCU
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -35,7 +35,7 @@ def ds_simulator_opcua_server_mock_fixture():
     simulator_process.wait()  # Wait for the process to terminate completely
 
 
-class StubScu(SCU):
+class StubScu(_SCU):
     """High level library stub class (no real subscriptions)."""
 
     subscriptions: dict = {}
