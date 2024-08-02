33c99f6a83099eac5016e3cd450a8fbd0678a4f4
Ulrik Pedersen <ukp@observatorysciences.co.uk>
2024-08-02 11:52:44 +0100

    WOM-409: disq.__init__.py add public types to __all__

--- src/disq/__init__.py
+++ src/disq/__init__.py
@@ -2,15 +2,18 @@
 
 from importlib.metadata import version  # noqa
 
-from disq.constants import Command, ResultCode  # noqa: F401
-from disq.sculib import (  # noqa: F401
-    SCU,
-    SCU_from_config,
-    SecondaryControlUnit,
-    configure_logging,
-)
+from disq.constants import Command, ResultCode
+from disq.sculib import SCU, SCU_from_config, SecondaryControlUnit, configure_logging
 
 __version__ = version("DiSQ")
 del version
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "Command",
+    "ResultCode",
+    "SCU",
+    "SCU_from_config",
+    "SecondaryControlUnit",
+    "configure_logging",
+]
