8e2d774cff1accefafa627378da8f4d302099ffb
Ulrik Pedersen <ukp@observatorysciences.co.uk>
2024-08-02 10:22:40 +0100

    WOM-409: disq.__init__.py imports significant public functions and types
    This simplifies the user API as the main types can be imported directly from disq

--- src/disq/__init__.py
+++ src/disq/__init__.py
@@ -2,6 +2,14 @@
 
 from importlib.metadata import version  # noqa
 
+from disq.constants import Command, ResultCode  # noqa: F401
+from disq.sculib import (  # noqa: F401
+    SCU,
+    SCU_from_config,
+    SecondaryControlUnit,
+    configure_logging,
+)
+
 __version__ = version("DiSQ")
 del version
 
